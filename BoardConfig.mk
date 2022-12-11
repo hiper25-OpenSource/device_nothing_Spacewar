35dd392ae60ffe6c45302ff26de7f214b2f11a51
hiper25 <2243168976@qq.com>
2022-12-11 19:28:04 +0000

    Spacewar: Set selinux neverallows
    Signed-off-by: hiper25 <2243168976@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,6 +156,8 @@ VENDOR_SECURITY_PATCH := 2022-09-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+IGNORE_SELINUX_NEVERALLOWS := true
+
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
